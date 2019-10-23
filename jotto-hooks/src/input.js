313fa93fc3a5433c94aef286197ae93ac50bff31
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-10-23 21:54:44 +1300

    written tests for input test submit button and input

--- jotto-hooks/src/input.js
+++ jotto-hooks/src/input.js
@@ -1,7 +1,37 @@
 import React from "react";
+import propTypes from "prop-types";
 
-const Input = () => {
-  return <div data-test="component-input"></div>;
+const Input = props => {
+  // we do not destruct hooks because they break our tests yo!
+  const [currentGuess, setCurrentGuess] = React.useState("");
+
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+          onChange={e => setCurrentGuess(e.target.value)}
+        ></input>
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          onClick={evt => {
+            evt.preventDefault();
+            setCurrentGuess("");
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+Input.propTypes = {
+  secretWord: propTypes.string.isRequired
 };
 
 export default Input;
--- jotto-hooks/src/input.test.js
+++ jotto-hooks/src/input.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 import Input from "./input";
 
 /**
@@ -8,12 +8,47 @@ import Input from "./input";
  * @function setup
  * @returns {ShallowWrapper}
  */
-const setup = () => shallow(<Input />);
+
+const setup = (secretWord = "party") =>
+  shallow(<Input secretWord={secretWord} />);
 
 describe("Input", () => {
   it("should be mounted without any errors", () => {
     const wrapper = setup();
     const input = findByTestAttr(wrapper, "component-input");
     expect(input.length).toBe(1);
+  });
+
+  it("on correct props, should not give any warnings", () => {
+    checkProps(Input, { secretWord: "party" });
+  });
+});
+
+describe("state controlled input field", () => {
+  let mockSetCurrentGuess = jest.fn();
+  let wrapper;
+
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    //overriding react useState
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+    wrapper = setup();
+  });
+
+  it("updates with value input box upon change", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+
+  it("is submmited with an empty string", () => {
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+
+    // we need prevent default to be passed from the test
+    submitButton.simulate("click", { preventDefault() {} });
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
   });
 });
